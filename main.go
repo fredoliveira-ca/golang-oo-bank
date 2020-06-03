e40c0aa717500e8c1b4d339e3cbf4f7ad4610dd5
Fred Oliveira <s2it_faquino@uolinc.com>
2020-06-03 08:46:19 -0300

    Creating the main.go

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type CheckingAccount struct {
+	name 			string
+	branch 			int
+	account 		int
+	accountDigit 	int
+	balance 		float64
+}
+
+func main() {
+	checkingAccount := CheckingAccount{
+		name: "Fred Oliveira",
+		branch: 1234,
+		account: 58096,
+		accountDigit: 1,
+		balance: 897.32}
+
+	checkingAccount1 := CheckingAccount{
+		name: "Fred Oliveira",
+		balance: 897.32}
+	
+	checkingAccount2 := CheckingAccount{ "Fred Oliveira", 1234, 58096, 1, 897.32 }
+	
+
+	fmt.Println(checkingAccount, checkingAccount1, checkingAccount2)
+
+
+}
