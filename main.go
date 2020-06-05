043532cd57325be9642be6d83c7f0b5a01e8e047
Fred Oliveira <fredoliveira.ca@gmail.com>
2020-06-04 22:54:31 -0300

    Creating withdraw function.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// CheckingAccount is a representation of a account type
 type CheckingAccount struct {
 	name 			string
 	branch 			int
@@ -13,28 +14,21 @@ type CheckingAccount struct {
 }
 
 func main() {
-	checkingAccount := CheckingAccount{
-		name: "Fred Oliveira",
-		branch: 1234,
-		account: 58096,
-		accountDigit: 1,
-		balance: 897.32}
+	account := CheckingAccount{}
+	account.name = "Fred"
+	account.balance = 500
 
-	checkingAccount1 := CheckingAccount{
-		name: "Fred Oliveira",
-		balance: 897.32}
-	
-	checkingAccount2 := CheckingAccount{ "Fred Oliveira", 1234, 58096, 1, 897.32 }
-	
-	fmt.Println(checkingAccount, checkingAccount1, checkingAccount2)
+	fmt.Println(account.Withdraw(600))
 
-	var someoneAccount *CheckingAccount
-	someoneAccount = new(CheckingAccount)
-	someoneAccount.name = "Jeseph"
-	someoneAccount.balance = 432.12
+}
 
-	fmt.Println(&someoneAccount)
-	fmt.Println(*someoneAccount)
-	fmt.Println(someoneAccount)
-
+// Withdraw is a function for withdraw money from the balance
+func (account *CheckingAccount) Withdraw(value float64) string {
+	isValidWithdraw := value > 0 && value <= account.balance
+	if(isValidWithdraw) {
+		account.balance -= value
+		return "Success!" + " Your new balance is: " + fmt.Sprintf("%f", account.balance)
+	} else {
+		return "No sufficient balance." + " Your new balance is: " + fmt.Sprintf("%f", account.balance)
+	}
 }
