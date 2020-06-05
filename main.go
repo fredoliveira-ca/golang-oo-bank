db78bacdd6a33a2c223dd2c79e495a7e86f93c56
Fred Oliveira <fredoliveira.ca@gmail.com>
2020-06-05 18:14:21 -0300

    Creating transfer function.

--- main.go
+++ main.go
@@ -18,28 +18,42 @@ func main() {
 	account.name = "Fred"
 	account.balance = 500
 
+	account2 := CheckingAccount{name: "Fred", balance: 500}
+
 	fmt.Println(account.Withdraw(600))
 	fmt.Println(account.Deposit(200))
 	fmt.Println(account.Withdraw(600))
+	fmt.Println(account.Transfer(100, &account2))
 }
 
-// Withdraw is a function for withdraw money from the balance
+// Withdraw is a function to withdraw money from the balance
 func (account *CheckingAccount) Withdraw(value float64) (string, string, float64) {
 	isValidWithdraw := value > 0 && value <= account.balance
 	if(isValidWithdraw) {
 		account.balance -= value
-		return "Success!", "Your balance was updated.", account.balance
-	} else {
-		return "Failure", "No sufficient balance.", account.balance
+		return "Success Withdraw!", "Your balance was updated. Balance", account.balance
 	}
+	
+	return "Failure Withdraw!", "No sufficient balance. Balance", account.balance
 }
 
-// Deposit is a function for deposit money into the balance
+// Deposit is a function to deposit money into the balance
 func (account *CheckingAccount) Deposit(value float64) (string, string, float64) {
 	if value > 0 {
 		account.balance += value
-		return "Success!", "Your balance was updated.", account.balance
-	} else {
-		return "Failure", "No sufficient balance.", account.balance
-	}
+		return "Success Deposit!", "Your balance was updated. Balance", account.balance
+	} 
+
+	return "Failure Deposit!", "No sufficient balance. Balance", account.balance
+}
+
+// Transfer is a function to tranfer money between accounts
+func (account *CheckingAccount) Transfer(value float64, targetAccount *CheckingAccount) (string, string, float64) {
+	if account.balance >= value && value > 0 {
+		account.balance -= value
+		targetAccount.Deposit(value)
+		return "Success Transfer!", "Your balance was updated. Balance", account.balance
+	} 
+	
+	return "Failure Transfer!", "No sufficient balance. Balance", account.balance
 }
---
+++ testing-structs.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func creatingAccount() {
+	checkingAccount := CheckingAccount{
+		name: "Fred Oliveira",
+		branch: 1234,
+		account: 58096,
+		accountDigit: 1,
+		balance: 897.32}
+	
+	checkingAccount1 := CheckingAccount{
+		name: "Fred Oliveira",
+		balance: 897.32}
+	
+	checkingAccount2 := CheckingAccount{ "Fred Oliveira", 1234, 58096, 1, 897.32 }
+	
+	fmt.Println(checkingAccount, checkingAccount1, checkingAccount2)
+	
+	var someoneAccount *CheckingAccount
+	someoneAccount = new(CheckingAccount)
+	someoneAccount.name = "Jeseph"
+	someoneAccount.balance = 432.12
+	
+	fmt.Println(&someoneAccount)
+	fmt.Println(*someoneAccount)
+	fmt.Println(someoneAccount)
+}
+
